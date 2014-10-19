628af2f7468b5f1ee9d9108ac72cf8ce786c6412
lennycartier <l.cartier@free.fr>
2014-10-19 23:01:09 +0200

    Use mainstream puppet call

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -1,23 +1,27 @@
 Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
 
+exec { 'add-nodesource-repo':
+	command => "curl -sL https://deb.nodesource.com/setup | sudo bash -",
+}
+
 class system-update {
-  exec { 'apt-get update':
-    command => 'apt-get update',
+	exec { 'apt-get update':
+		command => 'apt-get update',
   }
 }
 
 class system-upgrade {
-    exec { 'apt-get upgrade':
-        command => 'apt-get upgrade -y',
+	exec { 'apt-get upgrade':
+        	command => 'apt-get upgrade -y',
     }
 }
 
-class packages {
-    exec { 'apt-get install':
-        command => 'apt-get install -y nodejs npm',
+class js_packages {
+	package { "nodejs":
+    		ensure => "latest"
     }
 }
 
 include system-update
 include system-upgrade
-include packages
+include js_packages
